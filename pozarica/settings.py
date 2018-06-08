48321df1022e61a4ac668014fa854a8c13e9031b
Dagorik <dagorik@gmail.com>
2018-06-08 16:36:12 -0700

    add requierements

--- pozarica/settings.py
+++ pozarica/settings.py
@@ -131,6 +131,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '../pozarica/static'),)
+
