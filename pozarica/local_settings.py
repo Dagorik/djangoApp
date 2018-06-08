f7dabc05400ba4549984f0a1736b260c392fc8dd
Dagorik <dagorik@gmail.com>
2018-06-08 16:45:10 -0700

    add requierements

--- pozarica/local_settings.py
+++ pozarica/local_settings.py
@@ -13,3 +13,4 @@ DATABASES = {
 }
 
 DEBUG = True
+
--- pozarica/settings.py
+++ pozarica/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x+&%ccruwk^9^iwtp==vt0m2*52=a_w1@8v(iiw74vzo=q)3qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
@@ -128,7 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static'),
+
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), "static", "static" )
+try:
+     from .local_settings import *
+
+except:
+    pass
